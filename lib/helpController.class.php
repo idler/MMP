03020d1b124ceaeeb12dec333c03386a55a5cb36
Max Antonov <max.antonoff@gmail.com>
2009-12-11 20:47:44 +0300

    Added ignores, coloring help output

--- lib/helpController.class.php
+++ lib/helpController.class.php
@@ -7,8 +7,18 @@ class helpController extends AbstractController
   public function runStrategy()
   {
     echo <<<HELP
-This is the test help
+
+\033[41m \033[1m
+    This is the test help
+
+\033[40m\033[0m
 
 HELP;
+/*
+    $text ="echo This is the test help";
+    printf("\%s[%sm %s\n", chr(27), 43, $text );
+    */
+
+
   }
 }
