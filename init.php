35d7b4bab9733671fa04a52a7ea2ec81950b5fea
Max Antonov <max.antonoff@gmail.com>
2009-12-14 02:27:50 +0300

    Added ability for link migration.php in other directory and run with local db folder

--- init.php
+++ init.php
@@ -3,8 +3,8 @@
 set_include_path(__DIR__);
 function mmpAutoload($class)
 {
-  if(!file_exists('lib/'.$class.'.class.php')) throw new Exception("# class {$class} not found \n");
-  require_once 'lib/'.$class.'.class.php';
+  if(!file_exists(__DIR__.'/lib/'.$class.'.class.php')) throw new Exception("# class {$class} not found \n");
+  require_once __DIR__.'/lib/'.$class.'.class.php';
 }
 
 spl_autoload_register('mmpAutoload');
