2bc38e4f35ac170bf2398a6b7ae3321c7e67950c
Max Antonov <max.antonoff@gmail.com>
2010-02-11 23:32:51 +0300

    help coloring

--- lib/helpController.class.php
+++ lib/helpController.class.php
@@ -8,7 +8,7 @@ class helpController extends AbstractController
   {
     echo <<<HELP
 
-\033[31m\033[46m                    MySQL Migration with PHP                         \033[0m
+\033[1;34m\033[41m                    MySQL Migration with PHP                         \033[0m
 ---------------------------------------------------------------------
   \033[32mhelp:\033[0m       display this help and exit
   \033[32mschema:\033[0m     create schema for initial migration/installation
