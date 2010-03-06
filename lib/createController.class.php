245b5db2b5d1380f7df8b87e4e0422237eaefa63
Max Antonov <max.antonoff@gmail.com>
2010-03-07 01:37:16 +0300

    Prevent to write migration file, if no difference found

--- lib/createController.class.php
+++ lib/createController.class.php
@@ -1,25 +1,30 @@
 <?php
-require_once __DIR__.'/AbstractController.class.php';
+require_once __DIR__ . '/AbstractController.class.php';
 
 class createController extends AbstractController
 {
-
+  
   protected $queries = array();
-
+  
   public function runStrategy()
   {
-    
+
     $db = Factory::getDbObject();
     $tmpdb = Factory::getTmpDbObject();
 
     Factory::loadTmpDb($tmpdb);
 
-    $diff = new dbDiff($db,$tmpdb);
+    $diff = new dbDiff($db, $tmpdb);
     $difference = $diff->getDifference();
+    if (!count($difference['up']) && !count($difference['down']))
+    {
+      echo "Your database have no changes from last version\n";
+      exit(0);
+    }
 
     $version = Factory::getCurrentVersion();
-    $filename = Factory::get('savedir')."/migration{$version}.php";
-    $content = Factory::createMigrationContent($version,$difference);
+    $filename = Factory::get('savedir') . "/migration{$version}.php";
+    $content = Factory::createMigrationContent($version, $difference);
     file_put_contents($filename, $content);
     Factory::verbose("file: {$filename} writed!");
     $vTab = Factory::get('versiontable');
