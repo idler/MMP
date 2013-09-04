afd26302bb2aa9fdb7a20cecad15eede561359af
Arbuzov Sergey <info@whitediver.com>
2013-09-05 00:20:55 +0400

    Signed-off-by: Arbuzov Sergey <info@whitediver.com>

---
+++ lib/addController.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * @author  Arbuzov <info@whitediver.com>
+ *
+ */
+class addController extends AbstractController
+{
+	public function runStrategy()
+	{
+		Helper::initVersionTable();
+	}
+}
