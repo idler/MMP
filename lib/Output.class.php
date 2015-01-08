8c42538d1a9380d8c7727c7972560f447eec1694
Arbuzov Sergey <info@whitediver.com>
2015-01-08 17:27:45 +0400

    Change spaces to tabs
    Signed-off-by: Arbuzov Sergey <info@whitediver.com>

--- lib/Output.class.php
+++ lib/Output.class.php
@@ -1,15 +1,16 @@
 <?php
+
 class Output
 {
-
-    static function verbose ($msg, $level = 1)
-    {
-        if (intval(Helper::get('verbose')) >= $level)
-            echo $msg, PHP_EOL;
-    }
-
-    static function error ($msg)
-    {
-        fwrite(STDERR, $msg . PHP_EOL);
-    }
+  static function verbose($msg, $level = 1)
+  {
+    if(intval(Helper::get('verbose')) >= $level) echo $msg, PHP_EOL;
+  }
+  
+  static function error($msg)
+  {
+    fwrite(STDERR, $msg . PHP_EOL);
+  }
 }
+
+?>
