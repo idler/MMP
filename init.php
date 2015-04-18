027be4c128c81129d4bdab26fba3bca444ad21c2
pete <zzanzare@gmail.com>
2015-04-18 14:10:01 +0200

    proper composer autoloading

--- init.php
+++ init.php
@@ -1,24 +1,3 @@
 <?php
 
-define('DIR_SEP', DIRECTORY_SEPARATOR);
-
-set_include_path(__DIR__ . PATH_SEPARATOR . __DIR__ . DIR_SEP . 'lib' . DIR_SEP);
-
-spl_autoload_register('mmpAutoload');
-
-function mmpAutoload($class)
-{
-  #if(!class_exists(__DIR__.'/lib/'.$class.'.class.php'))
-  $paths = explode(PATH_SEPARATOR, get_include_path());
-
-  while($path = array_shift($paths))
-  {
-    $filename = $path . DIR_SEP . $class . '.class.php';
-    if(file_exists($filename))
-    {
-      require_once $filename;
-      return true;
-    }
-  }
-}
-
+require __DIR__ . '/vendor/autoload.php';
